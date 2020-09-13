c7257d48106542276b458143271cd811561b1627
BuildTools <unconfigured@null.spigotmc.org>
2020-09-13 06:13:15 -0400

    Made this faster

--- DashBlox/js/pages/themes.js
+++ DashBlox/js/pages/themes.js
@@ -1,32 +1,30 @@
 // Library \\
 var Roblox = Roblox || {};
 
-$(async () => {
+$(".icon-robux-28x28.roblox-popover-close").ready(async () => {
   let SettingEnabled = await Roblox.storage.CheckValue("oldicons");
-
+  
   if (SettingEnabled){
-    $(".icon-robux-28x28.roblox-popover-close").ready(() => {
-      try {
-        var RobuxIcon = $(".icon-robux-28x28.roblox-popover-close");
+    try {
+      var RobuxIcon = $(".icon-robux-28x28.roblox-popover-close");
 
-        RobuxIcon.removeClass("icon-robux-28x28");
-        RobuxIcon.addClass("icon-nav-robux");
-        //$(".gotham-font").removeClass("gotham-font");
+      RobuxIcon.removeClass("icon-robux-28x28");
+      RobuxIcon.addClass("icon-nav-robux");
+      //$(".gotham-font").removeClass("gotham-font");
 
-        //if ($(".border-top.border-bottom.game-stats-container.follow-button-enabled")){
-        //  $(".border-top.border-bottom.game-stats-container.follow-button-enabled").addClass("gotham-font");
-        //}
+      //if ($(".border-top.border-bottom.game-stats-container.follow-button-enabled")){
+      //  $(".border-top.border-bottom.game-stats-container.follow-button-enabled").addClass("gotham-font");
+      //}
 
-        //if ($(".list-item.item-card.recommended-item.ng-scope")){
-        //    $(".list-item.item-card.recommended-item.ng-scope").addClass("gotham-font");
-        //}
+      //if ($(".list-item.item-card.recommended-item.ng-scope")){
+      //    $(".list-item.item-card.recommended-item.ng-scope").addClass("gotham-font");
+      //}
 
-        $(".font-header-2.nav-menu-title.text-header")[1].text = "Catalog";
-        $(".btn-growth-md.btn-secondary-md")[0].text = "Upgrades";
-      } catch {
-        console.log("Page dosn't support themes.");
-      }
-    })
+      $(".font-header-2.nav-menu-title.text-header")[1].text = "Catalog";
+      $(".btn-growth-md.btn-secondary-md")[0].text = "Upgrades";
+    } catch {
+      console.log("Page dosn't support themes.");
+    }
   }
 })
 // This needs moved because this was originally a developer feature. \\
