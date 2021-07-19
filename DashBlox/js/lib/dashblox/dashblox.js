6de7cd0aef17199c9fd6fb70f1707876526e641b
WingedDash <eaevans113@gmail.com>
2021-07-19 16:56:18 -0400

    Util updates

--- DashBlox/js/lib/dashblox/dashblox.js
+++ DashBlox/js/lib/dashblox/dashblox.js
@@ -1,7 +1,5 @@
 "use strict"
 
-const dashblox = {};
-
 const runtime = chrome.runtime;
 const manifest = runtime.getManifest();
 
@@ -9,36 +7,43 @@ const developerMode = (manifest.short_name === "DashBlox_dev");
 
 const storage = chrome.storage.local;
 
-dashblox.get = ((url, data) => {
-    return new Promise((resolve, reject) => {
-        let message = {
-            url: url,
-            data: data,
-            request: "get"
-        }
+const dashblox = {
+    get: (url, data) => {
+        let message = {url: url, data: data, request: "get"};
 
-        runtime.sendMessage(message, (response) => {
-            if (response) {
-                resolve(response);
-            }
-            reject([{code: 0, message: "an error accoured getting your request."}]);
+        return new Promise((resolve, reject) => {
+            runtime.sendMessage(message, (response) => {
+                if (response) {
+                    resolve(response);
+                }
+                reject({message: "A error occurred while fetching your request."});
+            })
         })
-    })
-})
+    },
 
-dashblox.post = ((url, data) => {
-    return new Promise((resolve, reject) => {
-        let message = {
-            url: url,
-            data: data,
-            request: "post"
-        }
+    post: (url, data) => {
+        let message = {url: url, data: data, request: "post"};
 
-        runtime.sendMessage(message, (response) => {
-            if (response) {
-                resolve(response);
-            }
-            reject([{code: 0, message: "an error accoured getting your request."}]);
+        return new Promise((resolve, reject) => {
+            runtime.sendMessage(message, (response) => {
+                if (response) {
+                    resolve(response);
+                }
+                reject({message: "A error occurred while posting your request."});
+            })
         })
-    })
-})
+    },
+
+    ajax: (data) => {
+        let message = {data: data, request: "ajax"};
+
+        return new Promise((resolve, reject) => {
+            runtime.sendMessage(message, (response) => {
+                if (response) {
+                    resolve(response);
+                }
+                reject({message: "A error occurred while pushing your request."});
+            })
+        })
+    }
+}
