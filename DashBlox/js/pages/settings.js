232a01d098ce37f9b10ad98e6a57b17a2ef04551
WingedDash <eaevans113@gmail.com>
2021-07-31 12:03:01 -0400

    Working on settings, and created a new promo

--- DashBlox/js/pages/settings.js
+++ DashBlox/js/pages/settings.js
@@ -1,7 +1,44 @@
 "use strict"
 
 const settings = {
-    
+    general: {
+
+    },
+
+    catalog: {
+
+    },
+
+    assets: {
+
+    },
+
+    theme: {
+
+    },
+
+    setupComplete: false
+}
+
+async function setupSettings() {
+    function setSettings(setSetting) {
+        for (let setting in setSetting) {
+            if (typeof(setSetting[setting]) == 'object') {
+                setSettings(setSetting[setting]);
+            } else {
+                settings[setting] = setSetting[setting]
+            }
+        }
+    }
+
+    let getSettings = await dashblox.storage.get("settings");
+    let currentSettings = getSettings.settings;
+
+    if (!currentSettings) {
+        dashblox.storage.save("settings", settings);
+    } else {
+        setSettings(currentSettings);
+    }
 }
 
 pages.settings = () => {
@@ -11,3 +48,5 @@ pages.settings = () => {
         $("title")[0].text = "Settings - DashBlox";
     })
 }
+
+setupSettings();
