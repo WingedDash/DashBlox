13dc673ef52e05665e32dc61f0b8cdb867658f34
WingedDash <eaevans113@gmail.com>
2021-08-10 00:42:15 -0400

    Added Most Recent Items to catalog tabs

--- DashBlox/js/pages/catalog.js
+++ DashBlox/js/pages/catalog.js
@@ -4,4 +4,8 @@ pages.catalog = () => {
     $.watch(".heading.ng-scope", () => {
         $("#main-view > .search-bars > .heading.ng-scope > a")[0].text = "Catalog";
     })
+
+    $.watch(".font-header-2.text-subheader.panel.panel-default.ng-scope", () => {
+        $($(".font-header-2.text-subheader.panel.panel-default.ng-scope")[1]).after(`<li class="font-header-2 text-subheader panel panel-default ng-scope"> <a id="expandable-menu-category-1" class="small text menu-link text-link-secondary panel-heading"> <a class="small text menu-link text-link-secondary panel-heading" href="https://www.roblox.com/catalog?Category=1&amp;CreatorID=1&amp;SortType=3&amp;IncludeNotForSale">Most Recent Items</a> <span class="toggle-submenu ng-hide icon-minus"></span> </a> </li>`);
+    })
 }
