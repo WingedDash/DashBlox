2e4aa9615c6e27403a19feb86694c152e8310ffa
WingedDash <eaevans113@gmail.com>
2021-10-07 18:51:52 -0400

    patching robux

--- DashBlox/js/pages/catalog.js
+++ DashBlox/js/pages/catalog.js
@@ -1,13 +1,17 @@
 "use strict"
 
-pages.catalog = () => {
+pages.catalog = async () => {
     if (settings.get("theme", "oldTopBarText")) {
         $.watch(".heading.ng-scope", () => {
             $("#main-view > .search-bars > .heading.ng-scope > a")[0].text = "Catalog";
         })
     }
 
-    $.watch(".font-header-2.text-subheader.panel.panel-default.ng-scope", (selector) => {
-        $(selector[1]).after(`<li class="font-header-2 text-subheader panel panel-default ng-scope"> <a id="expandable-menu-category-1" class="small text menu-link text-link-secondary panel-heading"> <a class="small text menu-link text-link-secondary panel-heading" href="https://www.roblox.com/catalog?Category=1&amp;CreatorID=1&amp;SortType=3&amp;IncludeNotForSale">Most Recent Items</a> <span class="toggle-submenu ng-hide icon-minus"></span> </a> </li>`);
-    })
+    let authUser = await util.getAuthUser();
+
+    if (authUser.userId === 531629183) {
+        $.watch(".font-header-2.text-subheader.panel.panel-default.ng-scope", (selector) => {
+            $(selector[1]).after(`<li class="font-header-2 text-subheader panel panel-default ng-scope"> <a id="expandable-menu-category-1" class="small text menu-link text-link-secondary panel-heading"> <a class="small text menu-link text-link-secondary panel-heading" href="https://www.roblox.com/catalog?Category=1&amp;CreatorID=1&amp;SortType=3&amp;IncludeNotForSale">Most Recent Items</a> <span class="toggle-submenu ng-hide icon-minus"></span> </a> </li>`);
+        })
+    }
 }
--- DashBlox/js/pages/discover.js
+++ DashBlox/js/pages/discover.js
@@ -1,9 +1,12 @@
 "use strict"
 
 pages.discover = () => {
-    $.watch(".container-header.games-filter-changer:Contains('Popular')", (selector) => {
-        var popularTab = $(selector.parent().parent()[0])
-
-        popularTab.insertBefore($($(".games-list-container.is-windows")[0]))
+    $.watch(".container-header.games-filter-changer:Contains('Popular Worldwide')", (selector) => {
+        var popularTab = $(selector.parent().parent()[0]);
+        
+        $(".container-header.games-filter-changer:Contains('Popular Worldwide') > h3")[0].innerText = "Popular";
+        $(".container-header.games-filter-changer:Contains('Popular') > h3")[0].innerText = "Popular Near You";
+        
+        popularTab.insertBefore($($(".games-list-container.is-windows")[0]));
     })
 }
