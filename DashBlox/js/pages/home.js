beede736d266d92fb1895e01ab76f0dd821b4b16
WingedDash <eaevans113@gmail.com>
2021-10-08 02:21:13 -0400

    Robux symbol update

--- DashBlox/js/pages/home.js
+++ DashBlox/js/pages/home.js
@@ -5,7 +5,7 @@ pages.home = () => {
         $.watch(".font-header-1", () => {
             try {
                 $("#place-list > .game-home-page-container > .game-home-page-carousel-title > .font-header-1:Contains('Continue')")[0].innerText = "Continue Playing";
-                $("#place-list > .game-home-page-container > .game-home-page-carousel-title > .font-header-1:Contains('Friends Visiting')")[0].innerText = "Friends Playing";
+                $("#place-list > .game-home-page-container > .game-home-page-carousel-title > .font-header-1:Contains('Friend Activity')")[0].innerText = "Friends Playing";
             } catch (error) {
             }
         })
