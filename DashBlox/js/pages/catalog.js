ff4b428594163750f96f731df9ac8d2bf3788f89
WingedDash <eaevans113@gmail.com>
2021-11-20 17:02:31 -0500

    Making the most recent item viewer public

--- DashBlox/js/pages/catalog.js
+++ DashBlox/js/pages/catalog.js
@@ -7,11 +7,7 @@ pages.catalog = async () => {
         })
     }
 
-    let authUser = await util.getAuthUser();
-
-    if (authUser.userId === 531629183) {
-        $.watch(".font-header-2.text-subheader.panel.panel-default.ng-scope", (selector) => {
-            $(selector[1]).after(`<li class="font-header-2 text-subheader panel panel-default ng-scope"> <a id="expandable-menu-category-1" class="small text menu-link text-link-secondary panel-heading"> <a class="small text menu-link text-link-secondary panel-heading" href="https://www.roblox.com/catalog?Category=1&amp;CreatorID=1&amp;SortType=3&amp;IncludeNotForSale">Most Recent Items</a> <span class="toggle-submenu ng-hide icon-minus"></span> </a> </li>`);
-        })
-    }
+    $.watch(".font-header-2.text-subheader.panel.panel-default.ng-scope", (selector) => {
+        $(selector[1]).after(`<li class="font-header-2 text-subheader panel panel-default ng-scope"> <a id="expandable-menu-category-1" class="small text menu-link text-link-secondary panel-heading"> <a class="small text menu-link text-link-secondary panel-heading" href="https://${currentUrlPaths[2]}/catalog?Category=1&amp;CreatorID=1&amp;SortType=3&amp;IncludeNotForSale" onclick="setTimeout(() => {window.location.reload()})">Most Recent Items</a> <span class="toggle-submenu ng-hide icon-minus"></span> </a> </li>`);
+    })
 }
